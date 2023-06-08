85d66c9e05ae0a91a5d657e4ed1606978dd504b0
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2023-06-08 16:38:04 +0200

    its running
    shoutout @stefan-espant en @knetters

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 import express from "express";
 
-("use strict");
-
-// Json
-const fs = require("fs");
-
-let rawdata = fs.readFileSync("collections.json");
-let collections = JSON.parse(rawdata);
-console.log(collections);
-
-// Maak een nieuwe express app
+// Create a new Express app
 const app = express();
 
-// Stel in hoe we express gebruiken
+const collectionsJson =
+  "https://raw.githubusercontent.com/IvarSchuyt/de-correspondent/main/Course/Course/collections.json";
+
+// Set EJS as the template engine and specify the views directory
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the public directory
 app.use(express.static("public"));
 
-// Stel het poortnummer in en start express
-app.set("port", process.env.PORT || 8000);
-app.listen(app.get("port"), function () {
-  console.log(`Application started on http://localhost:${app.get("port")}`);
+// Create a route for the index page
+app.get("/", (request, response) => {
+  const message = "De Correspondent - Podcasts";
+
+  // Fetch the data from the url
+  fetchJson(collectionsJson).then((data) => {
+    response.render("index", { ...data, message });
+  });
 });
+
+// Set the port number and start the server
+const port = process.env.PORT || 4444;
+app.listen(port, () => {
+  console.log(`Application available on: http://localhost:${port}`);
+});
+
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+async function postJson(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  const responseData = await response.json();
+  return responseData;
+}
